590efeebf2b1405ae2cf66e780f02089b7644ddf
Kerstin <kerstinweibeler@gmail.com>
2023-04-10 20:03:10 +0000

    feat: Add list_display for admin panel products and categories

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from .models import Product, Category
 
-# Register your models here.
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    """
+    Admin Panel for Products
+    """
+
+    list_display = (
+        "sku",
+        "title",
+        "category",
+        "rating",
+        "image",
+    )
+
+    ordering = ("sku",)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    """
+    Admin Panel for Categories
+    """
+
+    list_display = ("friendly_name", "pk")
+
+    ordering = ("friendly_name",)
