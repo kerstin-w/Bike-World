ff4527ec67528888e6d3fd24ab96499e8fb590a8
Kerstin <kerstinweibeler@gmail.com>
2023-04-11 10:38:00 +0000

    feat: Add form to sort products and add query to sort in productlistview

--- products/views.py
+++ products/views.py
@@ -1,5 +1,5 @@
 from django.contrib import messages
-from django.db.models import Q
+from django.db.models import Q, Case, When, F, DecimalField
 from django.shortcuts import render
 from django.views.generic import ListView
 from .models import Product, Category
@@ -26,7 +26,35 @@ class ProductListView(ListView):
                 Q(bike_type__icontains=search_keyword)
             )
         return queryset
+    
+    def sort_queryset(self, queryset):
+        sort_by = self.request.GET.get('sort_by')
+        if sort_by == 'price_asc':
+            # Annotate the queryset with a new field named absolute_price,
+            # which will be used for sorting. This field is calculated 
+            # based on the sale price (if on sale) or the retail price. 
+            # Qqueryset is sorted in ascending order based on 
+            # the absolute_price field.
+            queryset = queryset.annotate(absolute_price=Case(
+                When(sale=True, then=F('sale_price')),
+                default=F('retail_price'),
+                output_field=DecimalField(),
+            )).order_by('absolute_price')
+        elif sort_by == 'price_desc':
+            # Queryset is sorted in descending order.
+            queryset = queryset.annotate(absolute_price=Case(
+                When(sale=True, then=F('sale_price')),
+                default=F('retail_price'),
+                output_field=DecimalField(),
+            )).order_by('-absolute_price')
+        elif sort_by == 'rating_desc':
+            # Sort the queryset by rating.
+            queryset = queryset.order_by('-rating')
+        else:
+            # Default sorting is by stock.
+            queryset = queryset.order_by('-stock')
 
+        return queryset
 
     def get_queryset(self):
         queryset = super().get_queryset()
@@ -43,7 +71,7 @@ class ProductListView(ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-
+        context['sort_by'] = self.request.GET.get('sort_by')
         paginator = context['paginator']
         page_obj = context['page_obj']
         context['products'] = paginator.get_page(page_obj.number)
