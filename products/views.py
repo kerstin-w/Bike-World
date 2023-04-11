99d1f184a88d109e2efe45b5b7fb4f012cc98961
Kerstin <kerstinweibeler@gmail.com>
2023-04-11 09:10:37 +0000

    feat: Create PorductListView, wire it up in urls and create simple template

--- products/views.py
+++ products/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Product
 
-# Create your views here.
+
+class ProductListView(ListView):
+    """
+    Displays a list of all products
+    """
+    model = Product
+    template_name = 'product_list.html'
+    paginate_by = 25
+    context_object_name = "products"
+    queryset = Product.objects.all()
