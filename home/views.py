2c7efd01538fcaee6710b6d58e695d6c717f796d
Kerstin <kerstinweibeler@gmail.com>
2023-04-11 13:14:03 +0000

    feat: Add context data to HomePageView to show toprated products

--- home/views.py
+++ home/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import TemplateView
+from products.models import Product
 
 
 class HomePageView(TemplateView):
@@ -6,3 +7,9 @@ class HomePageView(TemplateView):
     Render the Homepage
     """
     template_name = 'home/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        top_rated_products = Product.objects.filter(rating__isnull=False).order_by('-rating')[:4]
+        context['top_rated_products'] = top_rated_products
+        return context
