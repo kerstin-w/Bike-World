b4b7bcac43c73ef45369ee5b45ce747c9184cb6e
Kerstin <kerstinweibeler@gmail.com>
2023-04-11 17:32:20 +0000

    test: Create Tests for Category Admin. Test ordering of categories and test list_display

--- products/tests/test_admin.py
+++ products/tests/test_admin.py
@@ -9,6 +9,7 @@ class ProductAdminTestCase(TestCase):
     """
     Test for Product Admin
     """
+
     def setUp(self):
         """
         Test Data
@@ -19,7 +20,7 @@ class ProductAdminTestCase(TestCase):
             name="test_category", friendly_name="Test friendly name"
         )
         self.product_with_image = Product.objects.create(
-            image='path/to/image.jpg',
+            image="path/to/image.jpg",
             title="Product A",
             sku="SKU001",
             category=self.category,
@@ -54,9 +55,9 @@ class ProductAdminTestCase(TestCase):
             stock=5,
         )
 
-    def test_list_display(self):
+    def test_product_list_display(self):
         """
-        Test List Display in Admin Panel
+        Test List Display in Product Admin Panel
         """
         expected = (
             "sku",
@@ -68,40 +69,67 @@ class ProductAdminTestCase(TestCase):
         )
         self.assertEqual(self.product_admin.list_display, expected)
 
-    def test_ordering(self):
+    def test_product_ordering(self):
         """
         Test product order in Admin Panel
         """
         expected = ("sku",)
         self.assertEqual(self.product_admin.ordering, expected)
 
-    def test_list_filter(self):
+    def test_product_list_filter(self):
         """
-        Test list filter in Admin Panel
+        Test list filter in Product Admin Panel
         """
         expected = ("category", "sale", "gender", "brand")
         self.assertEqual(self.product_admin.list_filter, expected)
 
-    def test_search_fields(self):
+    def test_product_search_fields(self):
         """
-        Test search fields in Admin Panel
+        Test search fields in Product Admin Panel
         """
         expected = ["title", "sku"]
         self.assertEqual(self.product_admin.search_fields, expected)
 
-    def test_image_tag_with_image(self):
+    def test_product_image_tag_with_image(self):
         """
-        Test image_tag in Admin Panel
+        Test image_tag in Product Admin Panel
         """
         result = self.product_with_image.image_tag()
-        expected = '<img src="/media/path/to/image.jpg" style="width:150px;height:120px;object-fit:contain;">'
+        expected = '<img src="/media/path/to/image.jpg" ' \
+            'style="width:150px;height:120px;object-fit:contain;">'
         self.assertEqual(result, expected)
 
-    def test_image_tag_without_image(self):
+    def test_product_image_tag_without_image(self):
         """
         Test image_tag returns 'No Image Found' for product without image
         """
         result = self.product_without_image.image_tag()
-        expected = 'No Image Found'
+        expected = "No Image Found"
         self.assertEqual(result, expected)
 
+
+class CategoryAdminTestCase(TestCase):
+    """
+    Test for Category Admin
+    """
+
+    def setUp(self):
+        """
+        Test Data
+        """
+        self.site = AdminSite()
+        self.category_admin = CategoryAdmin(Category, self.site)
+
+    def test_category_list_display(self):
+        """
+        Test List Display in Category Admin Panel
+        """
+        expected = ("friendly_name", "pk")
+        self.assertEqual(self.category_admin.list_display, expected)
+
+    def test_category_ordering(self):
+        """
+        Test order Category in Admin Panel
+        """
+        expected = ("friendly_name",)
+        self.assertEqual(self.category_admin.ordering, expected)
