9e5e3136e332f0393d4327505b745165d9331435
Kerstin <kerstinweibeler@gmail.com>
2023-04-11 17:22:18 +0000

    test: Add Test Case for Product Admin and create test for list display

---
+++ products/tests/test_admin.py
@@ -0,0 +1,69 @@
+from django.contrib.admin.sites import AdminSite
+from django.test import TestCase
+
+from products.admin import ProductAdmin, CategoryAdmin
+from products.models import Product, Category
+
+
+class ProductAdminTestCase(TestCase):
+    """
+    Test for Product Admin
+    """
+    def setUp(self):
+        """
+        Test Data
+        """
+        self.site = AdminSite()
+        self.product_admin = ProductAdmin(Product, self.site)
+        self.category = Category.objects.create(
+            name="test_category", friendly_name="Test friendly name"
+        )
+        self.product_with_image = Product.objects.create(
+            image='path/to/image.jpg',
+            title="Product A",
+            sku="SKU001",
+            category=self.category,
+            description="Test description A",
+            wheel_size="26 inches",
+            retail_price=15.99,
+            sale_price=10.99,
+            sale=True,
+            rating=4,
+            brand="Scott",
+            bike_type="Mountain Bike",
+            gender=0,
+            material="Test Material A",
+            derailleur="Test Derailleur A",
+            stock=5,
+        )
+        self.product_without_image = Product.objects.create(
+            title="Product B",
+            sku="SKU002",
+            category=self.category,
+            description="Test description B",
+            wheel_size="26 inches",
+            retail_price=15.99,
+            sale_price=10.99,
+            sale=True,
+            rating=4,
+            brand="Scott",
+            bike_type="Mountain Bike",
+            gender=0,
+            material="Test Material B",
+            derailleur="Test Derailleur B",
+            stock=5,
+        )
+
+    def test_list_display(self):
+        """
+        Test List Display in Admin Panel
+        """
+        expected = (
+            "sku",
+            "title",
+            "category",
+            "rating",
+            "stock",
+            "image_tag",
+        )
+        self.assertEqual(self.product_admin.list_display, expected)
