5cfc49374986ca2edf701252473f708e6bf89874
Kerstin <kerstinweibeler@gmail.com>
2023-04-11 11:36:46 +0000

    feat: Add JS to handle filters. Filter for Category and Brand and allow all Filters and Sorting to happen at the same time

--- products/static/products/js/filters.js
+++ products/static/products/js/filters.js
@@ -1,8 +1,106 @@
 $(document).ready(function () {
 
+    // Get the "All" category and brand checkboxes
+    const categoryAllCheckbox = $('input[name="category_all"]');
+    const brandAllCheckbox = $('input[name="brand_all"]');
+
+    // Get the filter forms and selected filters container elements
+    const filterForms = $('form[method="get"]');
+    const selectedCategoryFiltersContainer = $('#selected-category-filter');
+    const selectedBrandFiltersContainer = $('#selected-brand-filter');
+
+    // When the user selects an option in the brand/category dropdown menus
+    $('.dropdown-menu input[type="checkbox"]').change(function () {
+        const currentCheckbox = $(this);
+        const currentName = currentCheckbox.attr('name');
+
+        // If a checkbox is being checked, uncheck all other checkboxes with the same name
+        if (currentCheckbox.prop('checked')) {
+            $(`.dropdown-menu input[type="checkbox"][name="${currentName}"]:checked`)
+                .not(currentCheckbox)
+                .prop('checked', false);
+        }
+
+        // Construct new URL with selected sort/filter parameters and redirect the user
+        const selectedSort = $('#sort-by').val();
+        const selectedCategories = $('input[name="category"]:checked').not(categoryAllCheckbox).map(function () {
+            return $(this).val();
+        }).get().join(',');
+        const selectedBrands = $('input[name="brand"]:checked').not(brandAllCheckbox).map(function () {
+            return $(this).val();
+        }).get().join(',');
+
+        const queryParams = {
+            sort_by: selectedSort,
+            category: selectedCategories,
+            brand: selectedBrands
+        };
+
+        const url = window.location.pathname + '?' + $.param(queryParams);
+        window.location.href = url;
+    });
+
+    // When the user clicks the "All" category checkbox
+    categoryAllCheckbox.change(function () {
+        const isChecked = $(this).prop('checked');
+        $('input[name="category"]:checked').not(categoryAllCheckbox).prop('checked', false);
+
+        // Ensure that the "All" checkbox always remains selected
+        if (!isChecked) {
+            $(this).prop('checked', true);
+        }
+
+        // Construct new URL with selected sort/filter parameters and redirect the user
+        const selectedSort = $('#sort-by').val();
+        const selectedCategories = $('input[name="category"]:checked').not(categoryAllCheckbox).map(function () {
+            return $(this).val();
+        }).get().join(',');
+        const selectedBrands = $('input[name="brand"]:checked').not(brandAllCheckbox).map(function () {
+            return $(this).val();
+        }).get().join(',');
+
+        const queryParams = {
+            sort_by: selectedSort,
+            category: selectedCategories,
+            brand: selectedBrands
+        };
+
+        const url = window.location.pathname + '?' + $.param(queryParams);
+        window.location.href = url;
+    });
+
+    // When the user clicks the "All" brand checkbox
+    brandAllCheckbox.change(function () {
+        const isChecked = $(this).prop('checked');
+        $('input[name="brand"]:checked').not(brandAllCheckbox).prop('checked', false);
+
+        // Ensure that the "All" checkbox always remains selected
+        if (!isChecked) {
+            $(this).prop('checked', true);
+        }
+
+        // Construct new URL with selected sort/filter parameters and redirect the user
+        const selectedSort = $('#sort-by').val();
+        const selectedCategories = $('input[name="category"]:checked').not(categoryAllCheckbox).map(function () {
+            return $(this).val();
+        }).get().join(',');
+        const selectedBrands = $('input[name="brand"]:checked').not(brandAllCheckbox).map(function () {
+            return $(this).val();
+        }).get().join(',');
+
+        const queryParams = {
+            sort_by: selectedSort,
+            category: selectedCategories,
+            brand: selectedBrands
+        };
+
+        const url = window.location.pathname + '?' + $.param(queryParams);
+        window.location.href = url;
+    });
+
     // When the user selects a different sorting option
     $('#sort-by').on('change', function () {
-        // Construct new URL with selected sort parameters and redirect the user
+        // Construct new URL with selected sort/filter parameters and redirect the user
         const selectedSort = $(this).val();
         const queryParams = $.extend({}, getCurrentQueryParams(), {
             sort_by: selectedSort
@@ -11,6 +109,10 @@ $(document).ready(function () {
         window.location.href = url;
     });
 
+    // When the user changes a filter
+    filterForms.on('change', function () {
+        updateSelectedFilters();
+    });
 
     // Get the current query parameters
     const getCurrentQueryParams = function () {
@@ -25,4 +127,58 @@ $(document).ready(function () {
         return queryParams;
     }
 
+    // Update the selected filters containers
+    const updateSelectedFilters = function () {
+        const queryParams = getCurrentQueryParams();
+        const selectedCategories = $('input[name="category"][type="checkbox"]:checked')
+            .map(function () {
+                if ($(this).val() !== 'all') {
+                    return $(this).next().text().trim();
+                }
+            })
+            .get();
+
+        const selectedBrands = $('input[name="brand"][type="checkbox"]:checked')
+            .map(function () {
+                return $(this).next().text().trim();
+            })
+            .get();
+
+        const selectedCategoryFilters = [];
+        const selectedBrandFilters = [];
+
+        // Add selected parameters to selected filters arrays
+        if (selectedCategories.length > 0 && selectedCategories.indexOf('All') === -1) {
+            for (const category of selectedCategories) {
+                selectedCategoryFilters.push(category);
+            }
+        }
+
+        if (selectedBrands.length > 0 && selectedBrands.indexOf('All') === -1) {
+            for (const brand of selectedBrands) {
+                selectedBrandFilters.push(brand);
+            }
+        }
+
+        // Update filter container elements with selected filters
+        if (selectedCategoryFilters.length > 0) {
+            selectedCategoryFiltersContainer.html(`<span>${selectedCategoryFilters.join(' | ')}</span> `);
+        } else {
+            selectedCategoryFiltersContainer.empty();
+        }
+
+        if (selectedBrandFilters.length > 0) {
+            selectedBrandFiltersContainer.html(`<span>${selectedBrandFilters.join(' | ')}</span>`);
+        } else {
+            selectedBrandFiltersContainer.empty();
+        }
+    }
+
+    // Set initial filter states on page load
+    const queryParams = getCurrentQueryParams();
+    const selectedSort = queryParams.sort_by || '';
+    $('#sort-by').val(selectedSort);
+    $('input[name="category"][value="all"]').prop('checked', true);
+    $('input[name="brand"][value="all"]').prop('checked', true);
+    updateSelectedFilters();
 });
