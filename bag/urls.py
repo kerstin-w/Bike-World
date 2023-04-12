68257b41313ce3ea033a0fe7f36e12a53c11e38a
Kerstin <kerstinweibeler@gmail.com>
2023-04-12 05:52:53 +0000

    feat: Create view to add items to the shopping bag and wire it up in urls

--- bag/urls.py
+++ bag/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import BagView
+from .views import BagView, AddToBagView
 
 
 urlpatterns = [
     path('', BagView.as_view(), name='view_bag'),
+    path('add/<item_id>', AddToBagView.as_view(), name='add_to_bag'),
 ]
--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,6 @@
 from django.views.generic import TemplateView
+from django.views import View
+from django.shortcuts import render, redirect
 
 
 class BagView(TemplateView):
@@ -6,3 +8,28 @@ class BagView(TemplateView):
     Render the Shopping Bag
     """
     template_name = 'bag/bag.html'
+
+
+class AddToBagView(View):
+    """
+    View to add items to the shopping bag
+    """
+    def post(self, request, item_id):
+        """
+        Add quantity of the product to the shopping bag
+        """
+
+        quantity = int(request.POST.get('quantity'))
+        redirect_url = request.POST.get('redirect_url')
+        bag = request.session.get('bag', {})
+
+        if item_id in list(bag.keys()):
+            bag[item_id] += quantity
+        else:
+            bag[item_id] = quantity
+
+        request.session['bag'] = bag
+
+        request.session['bag'] = bag
+        print(request.session['bag'])
+        return redirect(redirect_url)
