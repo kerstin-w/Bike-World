5118a84f2d04b27764988119c45c18d4a460a37b
Kerstin <kerstinweibeler@gmail.com>
2023-04-12 14:40:38 +0000

    fix: Missing / in urls for bag

--- bag/urls.py
+++ bag/urls.py
@@ -5,6 +5,6 @@ from .views import BagView, AddToBagView, AdjustBagView, RemoveItemFromBagView
 urlpatterns = [
     path('', BagView.as_view(), name='view_bag'),
     path('add/<item_id>', AddToBagView.as_view(), name='add_to_bag'),
-    path('adjust/<item_id>', AdjustBagView.as_view(), name='adjust_bag'),
-    path('remove/<item_id>', RemoveItemFromBagView.as_view(), name='remove_from_bag'),
+    path('adjust/<item_id>/', AdjustBagView.as_view(), name='adjust_bag'),
+    path('remove/<item_id>/', RemoveItemFromBagView.as_view(), name='remove_from_bag'),
 ]
