531ef622c424b2a6296cd9da6ce9d7bb01cbc976
Kerstin <kerstinweibeler@gmail.com>
2023-04-14 12:23:12 +0000

    feat: Add current_bag, total and stripe_total to checkout view

--- checkout/views.py
+++ checkout/views.py
@@ -2,16 +2,25 @@ from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
 
 from .forms import OrderForm
+from bag.context_processors import bag_contents
 
 
 def checkout(request):
     """
     View for Order Checkout
     """
+    # Get basket from session
     bag = request.session.get('bag', {})
     if not bag:
         messages.error(request, "There's nothing in your bag at the moment")
         return redirect(reverse('products'))
+
+    # Set current basket
+    current_bag = bag_contents(request)
+    # Set grand total
+    total = current_bag['grand_total']
+    # Set stripe total
+    stripe_total = round(total * 100)
 
     order_form = OrderForm()
     template = 'checkout/checkout.html'
