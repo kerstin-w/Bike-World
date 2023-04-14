14c718d3cc7235bf46ba9813f6c687c7ba0c8ea9
Kerstin <kerstinweibeler@gmail.com>
2023-04-14 09:12:27 +0000

    feat: Add OrderLineItem to Admin panel

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Order, OrderLineItem
 
-# Register your models here.
+
+class OrderLineItemAdminInline(admin.TabularInline):
+    """
+    Order Line Items Admin
+    """
+    model = OrderLineItem
+    readonly_fields = ('lineitem_total',)
