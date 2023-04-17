7f05ace95e6334c5034805fb643f5b2bae9fc604
Kerstin <kerstinweibeler@gmail.com>
2023-04-17 13:35:51 +0000

    feat: Add UserProfile to context in Profileview

--- profiles/views.py
+++ profiles/views.py
@@ -1,8 +1,21 @@
 from django.views.generic import TemplateView
+from django.shortcuts import get_object_or_404
+from .models import UserProfile
 
 
 class ProfileView(TemplateView):
-    ''''
+    '''
     View to render the Usre Profile
     '''
     template_name = 'profiles/profile.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        # Retrieve the user's profile
+        profile = get_object_or_404(UserProfile, user=self.request.user)
+
+        # Add the profile to the context
+        context['profile'] = profile
+
+        return context
