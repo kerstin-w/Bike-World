d954c0d996b0149a914aeeefde76432d72ef8bbf
Kerstin <kerstinweibeler@gmail.com>
2023-04-17 07:38:15 +0000

    feat: Create data model for UserProfile and add user_profile to Oders data model

--- checkout/models.py
+++ checkout/models.py
@@ -7,6 +7,7 @@ from django.db.models import Model, Sum
 from django_countries.fields import CountryField
 
 from products.models import Product
+from profiles.models import UserProfile
 
 
 class Order(Model):
@@ -15,6 +16,13 @@ class Order(Model):
     """
 
     order_number = models.CharField(max_length=10, null=False, editable=False)
+    user_profile = models.ForeignKey(
+        UserProfile,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="orders",
+    )
     full_name = models.CharField(
         max_length=settings.FULL_NAME_MAX_LENGTH, null=False, blank=False
     )
@@ -22,7 +30,7 @@ class Order(Model):
     phone_number = models.CharField(
         max_length=settings.PHONE_NUMBER_MAX_LENGTH, null=False, blank=False
     )
-    country = CountryField(blank_label='Country *', null=False, blank=False)
+    country = CountryField(blank_label="Country *", null=False, blank=False)
     postcode = models.CharField(
         max_length=settings.POSTCODE_MAX_LENGTH, null=True, blank=True
     )
