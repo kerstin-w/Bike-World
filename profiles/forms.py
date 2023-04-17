95f1b2c4c8d2c173c9c57df9a6186b6d52ddb481
Kerstin <kerstinweibeler@gmail.com>
2023-04-17 14:07:34 +0000

    feat: Add form for UserProfile

---
+++ profiles/forms.py
@@ -0,0 +1,37 @@
+from django import forms
+from .models import UserProfile
+
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        exclude = ("user",)
+
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders and classes, remove auto-generated
+        labels and set autofocus on first field
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            "default_postcode": "Postal Code",
+            "default_town_or_city": "Town or City",
+            "default_street_address1": "Street Address 1",
+            "default_street_address2": "Street Address 2",
+            "default_phone_number": "Phone Number",
+        }
+
+        # Set autofocus on the first field
+        self.fields["default_street_address1"].widget.attrs["autofocus"] = True
+        for field in self.fields:
+            if field != "default_country":
+                if self.fields[field].required:
+                    # Add a required asterisk to the placeholder
+                    placeholder = f"{placeholders[field]} *"
+                else:
+                    placeholder = placeholders[field]
+                self.fields[field].widget.attrs["placeholder"] = placeholder
+            # Add a class for styling
+            self.fields[field].widget.attrs["class"] = "border-black"
+            # Remove auto-generated labels
+            self.fields[field].label = False
