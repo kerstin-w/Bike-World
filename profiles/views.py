d2beb1ef3ded61b783127577412cea1d223115b6
Kerstin <kerstinweibeler@gmail.com>
2023-04-17 14:23:03 +0000

    feat: Add form_valid method and get profile order to Profile View

--- profiles/views.py
+++ profiles/views.py
@@ -1,21 +1,44 @@
-from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import FormView, ListView
 from django.shortcuts import get_object_or_404
+from django.urls import reverse_lazy
 from .models import UserProfile
+from .forms import UserProfileForm
 
 
-class ProfileView(TemplateView):
-    '''
-    View to render the Usre Profile
-    '''
-    template_name = 'profiles/profile.html'
+class ProfileView(LoginRequiredMixin, FormView, ListView):
+    """
+    View to render the user profile
+    """
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
+    template_name = "profiles/profile.html"
+    form_class = UserProfileForm
+    login_url = "/accounts/login/"
+    success_url = reverse_lazy("profile")
 
-        # Retrieve the user's profile
+    def get_queryset(self):
+        """
+        Get the UserProfile for the current user using the request object.
+        Then, return a queryset of orders related to the retrieved UserProfile.
+        """
         profile = get_object_or_404(UserProfile, user=self.request.user)
+        return profile.orders.all()
 
-        # Add the profile to the context
-        context['profile'] = profile
+    def get_form_kwargs(self):
+        """
+        Get and return keyword arguments to be passed to the form class.
+        Populate the form with the current user's profile information.
+        """
+        kwargs = super().get_form_kwargs()
+        kwargs["instance"] = get_object_or_404(
+            UserProfile, user=self.request.user
+        )
+        return kwargs
 
-        return context
+    def form_valid(self, form):
+        """
+        Called when form submission is successful.
+        Save the form data to the corresponding UserProfile model instance.
+        """
+        form.save()
+        return super().form_valid(form)
