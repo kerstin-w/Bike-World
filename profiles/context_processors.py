c6376ac80134584c9048bf28f185325c15b0ceab
Kerstin <kerstinweibeler@gmail.com>
2023-04-17 12:13:35 +0000

    feat: Create context processors for login and signup form

---
+++ profiles/context_processors.py
@@ -0,0 +1,13 @@
+from allauth.account.forms import LoginForm, SignupForm
+
+def login_tag(request):
+    """
+    Context processor that adds a login form to the context of every template that's rendered.
+    """
+    return {'logintag': LoginForm()}
+
+def signup_tag(request):
+    """
+    Context processor that adds a signup form to the context of every template that's rendered.
+    """
+    return {'signuptag': SignupForm()}
