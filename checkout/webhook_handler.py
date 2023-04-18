998834cd0809641dd56603b05f828b3927afa16e
Kerstin <kerstinweibeler@gmail.com>
2023-04-18 09:35:11 +0000

    feat: Add method to send a order confirmation mail to webhook handler

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -1,4 +1,7 @@
 from django.http import HttpResponse
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.conf import settings
 
 from .models import Order, OrderLineItem
 from products.models import Product
@@ -16,6 +19,25 @@ class StripeWH_Handler:
 
     def __init__(self, request):
         self.request = request
+
+    def _send_confirmation_email(self, order):
+        """
+        Send the user a confirmation email
+        """
+        # Get the customer email from the order object
+        customer_email = order.email
+        # Render the subject and body of the email using templates
+        # and the order object
+        subject = render_to_string(
+            "checkout/confirmation_emails/confirmation_email_subject.txt",
+            {"order": order},
+        )
+        body = render_to_string(
+            "checkout/confirmation_emails/confirmation_email_body.txt",
+            {"order": order, "contact_email": settings.DEFAULT_FROM_EMAIL},
+        )
+
+        send_mail(subject, body, settings.DEFAULT_FROM_EMAIL, [customer_email])
 
     def handle_event(self, event):
         """
@@ -102,7 +124,9 @@ class StripeWH_Handler:
                 attempt += 1
                 time.sleep(1)
         # If the order exists, return a success response
+        # And send confirmation mail
         if order_exists:
+            self._send_confirmation_email(order)
             return HttpResponse(
                 content=(
                     f'Webhook received: {event["type"]} '
@@ -145,6 +169,8 @@ class StripeWH_Handler:
                     status=500,
                 )
         # If the Order was successfully created, return a success response
+        # And send confirmation email
+        self._send_confirmation_email(order)
         return HttpResponse(
             content=(
                 f'Webhook received: {event["type"]} '
