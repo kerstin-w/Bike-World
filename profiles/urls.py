5c79ef551b4a26ff89840e50167fbdfb6d13ea80
Kerstin <kerstinweibeler@gmail.com>
2023-04-18 07:04:23 +0000

    feat: On Profile add collapseable section to toggle between order history and profile

--- profiles/urls.py
+++ profiles/urls.py
@@ -4,7 +4,7 @@ from .views import ProfileView, OrderHistoryView
 urlpatterns = [
     path("", ProfileView.as_view(), name="profile"),
     path(
-        "order_history/<order_number>",
+        "order_history/<order_number>/",
         OrderHistoryView.as_view(),
         name="order_history",
     ),
