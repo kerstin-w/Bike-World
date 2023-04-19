fd15b384162ff0841febadf53127e099f908cc9c
Kerstin <kerstinweibeler@gmail.com>
2023-04-19 08:33:37 +0000

    feat: Add signal for user_signed_up to send welcome mail

---
+++ profiles/signals.py
@@ -0,0 +1,18 @@
+from django.conf import settings
+from allauth.account.signals import user_signed_up
+from django.dispatch import receiver
+from django.core.mail import send_mail
+
+
+# Define a receiver function to handle the user_signed_up signal
+@receiver(user_signed_up)
+def send_welcome_email(sender, **kwargs):
+    """
+    Function to handle user_signed_up signal and send welcome mail
+    """
+    user = kwargs["user"]
+    subject = "Welcome to My Awesome Website"
+    message = f"Hi {user.username}, welcome to Our Awesome Website!"
+    from_email = settings.DEFAULT_FROM_EMAIL
+    recipient_list = [user.email]
+    send_mail(subject, message, from_email, recipient_list)
