fbc98196c6b79e0f545c2e2ea7e06e658185f312
Kerstin <kerstinweibeler@gmail.com>
2023-04-19 19:18:46 +0000

    refactor: Use common class to hide all collapse containers besides the one clicked for profile collapse

--- profiles/static/profiles/js/profile_btn_toggle.js
+++ profiles/static/profiles/js/profile_btn_toggle.js
@@ -1,27 +1,27 @@
 $(document).ready(function () {
-    // Select the buttons
+    // Select the collapse buttons
     let collapseInfoBtn = $('#collapseInfoBtn');
     let collapseHistoryBtn = $('#collapseHistoryBtn');
+    let collapseWishlistBtn = $('#collapseWishlistBtn');
+
+    // Select all the collapse containers
+    let collapseContainers = $('.collapse');
 
     // Add a click event listener to the collapse info button
     collapseInfoBtn.on('click', function () {
-        // Select the collapse history container
-        let collapseHistory = $('#collapseHistory');
-        // Check if the collapse history container has the 'show' class
-        if (collapseHistory.hasClass('show')) {
-            // If it does, remove the class to hide the container
-            collapseHistory.removeClass('show');
-        }
+        // Hide all the collapse containers except the one that was clicked on
+        collapseContainers.not('#collapseInfo').removeClass('show');
     });
 
     // Add a click event listener to the collapse history button
     collapseHistoryBtn.on('click', function () {
-        // Select the collapse info container
-        let collapseInfo = $('#collapseInfo');
-        // Check if the collapse info container has the 'show' class
-        if (collapseInfo.hasClass('show')) {
-            // If it does, remove the class to hide the container
-            collapseInfo.removeClass('show');
-        }
+        // Hide all the collapse containers except the one that was clicked on
+        collapseContainers.not('#collapseHistory').removeClass('show');
+    });
+
+    // Add a click event listener to the collapse wishlist button
+    collapseWishlistBtn.on('click', function () {
+        // Hide all the collapse containers except the one that was clicked on
+        collapseContainers.not('#collapseWishlist').removeClass('show');
     });
 });
