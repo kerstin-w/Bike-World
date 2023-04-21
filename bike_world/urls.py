b02ea2c3e5f334ce09e08531d7309be72f669a47
Kerstin <kerstinweibeler@gmail.com>
2023-04-21 10:08:01 +0000

    feat: Create support app, add to settings and urls

--- bike_world/urls.py
+++ bike_world/urls.py
@@ -19,7 +19,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
@@ -28,4 +27,5 @@ urlpatterns = [
     path('bag/', include('bag.urls')),
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
+    path('support/', include('support.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
