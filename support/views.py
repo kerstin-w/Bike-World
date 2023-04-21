0c24ab41826b066502938e42a763cc3ed77ef634
Kerstin <kerstinweibeler@gmail.com>
2023-04-21 11:35:00 +0000

    feat: Add pre-fill functionality to contact form. Adding the functionality to pre-fill the form fields with the user's email and name if they are logged in to ContactForm.

--- support/views.py
+++ support/views.py
@@ -3,7 +3,9 @@ from django.shortcuts import render, redirect
 from django.core.mail import send_mail
 from django.contrib import messages
 from django.conf import settings
+
 from .forms import ContactForm
+from profiles.models import UserProfile
 
 
 class ContactView(View):
@@ -12,16 +14,39 @@ class ContactView(View):
     and handles the from
     """
 
-    # handle the GET request for the contact page
     def get(self, request, *args, **kwargs):
+        """
+        handle the GET request for the contact page
+        """
         # Get the current URL of the page
         request.session["previous_page"] = request.META.get("HTTP_REFERER")
-        form = ContactForm()
-        # Render the contact page with an empty ContactForm
+        # Check if the user is authenticated
+        if request.user.is_authenticated:
+            # Get the UserProfile object of the logged-in user (if exists)
+            try:
+                user_profile = UserProfile.objects.get(user=request.user)
+            except UserProfile.DoesNotExist:
+                user_profile = None
+            # If a user profile exists, pre-fill the form fields
+            # with the stored data
+            if user_profile:
+                form = ContactForm(
+                    initial={
+                        "name": user_profile.default_full_name,
+                        "email": request.user.email,
+                    }
+                )
+            else:
+                form = ContactForm()
+        else:
+            form = ContactForm()
+        # Render the contact page with the initialized ContactForm
         return render(request, "support/contact.html", {"form": form})
 
-    # handle the POST request for the contact page
     def post(self, request, *args, **kwargs):
+        """
+        handle the POST request for the contact page
+        """
         form = ContactForm(request.POST)
         # Check if the submitted form data is valid
         if form.is_valid():
