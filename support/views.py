5b4cf27797ed39d79075488ebc913aa3347e695a
Kerstin <kerstinweibeler@gmail.com>
2023-04-21 11:50:07 +0000

    refactor: Use FormView for ContactView. The ContactView now uses the ContactForm as its form class, specifies the success_url to redirect to after a successful form submission, and overrides the form_valid method to send an email and add a success message. To pre-fill the form fields with the logged-in user's data (if authenticated), the get_form_kwargs method is overridden to set the initial data based on the UserProfile object. The get_success_url method is overridden to redirect to the previous page using the HTTP_REFERER header, or fallback to the success_url.

--- support/views.py
+++ support/views.py
@@ -1,73 +1,59 @@
-from django.views import View
-from django.shortcuts import render, redirect
-from django.core.mail import send_mail
+from django.views.generic.edit import FormView
+from django.urls import reverse_lazy
 from django.contrib import messages
+from django.core.mail import send_mail
 from django.conf import settings
 
 from .forms import ContactForm
 from profiles.models import UserProfile
 
 
-class ContactView(View):
-    """
-    Contact View that displays the contact from
-    and handles the from
-    """
+class ContactView(FormView):
+    template_name = "support/contact.html"
+    form_class = ContactForm
+    success_url = reverse_lazy("home")
 
-    def get(self, request, *args, **kwargs):
-        """
-        handle the GET request for the contact page
-        """
-        # Get the current URL of the page
-        request.session["previous_page"] = request.META.get("HTTP_REFERER")
+    def form_valid(self, form):
+        # Get the form data
+        name = form.cleaned_data["name"]
+        email = form.cleaned_data["email"]
+        subject = form.cleaned_data["subject"]
+        message = form.cleaned_data["message"]
+        # Send email
+        send_mail(
+            subject,
+            f"{name} wrote, \n\n{message}",
+            email,
+            [settings.DEFAULT_FROM_EMAIL],
+            fail_silently=False,
+        )
+        # Add a success message to be displayed after the redirect
+        messages.success(
+            self.request, "Thanks for contacting us! We will be in touch soon."
+        )
+        return super().form_valid(form)
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
         # Check if the user is authenticated
-        if request.user.is_authenticated:
+        if self.request.user.is_authenticated:
             # Get the UserProfile object of the logged-in user (if exists)
             try:
-                user_profile = UserProfile.objects.get(user=request.user)
+                user_profile = UserProfile.objects.get(user=self.request.user)
+                # Set the form's initial values to the user's name and email
+                kwargs["initial"] = {
+                    "name": user_profile.default_full_name,
+                    "email": self.request.user.email,
+                }
             except UserProfile.DoesNotExist:
-                user_profile = None
-            # If a user profile exists, pre-fill the form fields
-            # with the stored data
-            if user_profile:
-                form = ContactForm(
-                    initial={
-                        "name": user_profile.default_full_name,
-                        "email": request.user.email,
-                    }
-                )
-            else:
-                form = ContactForm()
-        else:
-            form = ContactForm()
-        # Render the contact page with the initialized ContactForm
-        return render(request, "support/contact.html", {"form": form})
+                pass
+        return kwargs
 
-    def post(self, request, *args, **kwargs):
-        """
-        handle the POST request for the contact page
-        """
-        form = ContactForm(request.POST)
-        # Check if the submitted form data is valid
-        if form.is_valid():
-            # Get the form data
-            name = form.cleaned_data["name"]
-            email = form.cleaned_data["email"]
-            subject = form.cleaned_data["subject"]
-            message = form.cleaned_data["message"]
-            # Send email
-            send_mail(
-                subject,
-                f"{name} wrote, \n\n{message}",
-                email,
-                [settings.DEFAULT_FROM_EMAIL],
-                fail_silently=False,
-            )
-            # Add a success message to be displayed after the redirect
-            messages.success(
-                request, "Thanks for contacting us! We will be in touch soon."
-            )
-            # Redirect to the previous page
-            return redirect(request.session.get("previous_page", "/"))
-        # Render the contact page with the invalid form and error messages
-        return render(request, "support/contact.html", {"form": form})
+    def get_success_url(self):
+        # Redirect to the previous page
+        return self.request.session.get("previous_page", self.success_url)
+
+    def get(self, request, *args, **kwargs):
+        # Store the previous page URL in the session
+        request.session["previous_page"] = request.META.get("HTTP_REFERER")
+        return super().get(request, *args, **kwargs)
