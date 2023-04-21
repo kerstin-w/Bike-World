252c32c1cab9dd41c4e345283d968adb4e440ab4
Kerstin <kerstinweibeler@gmail.com>
2023-04-21 10:49:57 +0000

    feat: Create View for Contact and wire it up in urls

--- support/views.py
+++ support/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+from django.views import View
+from django.urls import reverse
+from django.shortcuts import render, redirect
+from django.core.mail import send_mail
+from django.contrib import messages
+from django.conf import settings
+from .forms import ContactForm
 
-# Create your views here.
+
+class ContactView(View):
+    """
+    Contact View that displays the contact from
+    and handles the from
+    """
+
+    # handle the GET request for the contact page
+
+    def get(self, request, *args, **kwargs):
+        form = ContactForm()
+        # Render the contact page with an empty ContactForm
+        return render(request, "support/contact.html", {"form": form})
+
+    # handle the POST request for the contact page
+    def post(self, request, *args, **kwargs):
+        form = ContactForm(request.POST)
+        # Check if the submitted form data is valid
+        if form.is_valid():
+            # Get the form data
+            name = form.cleaned_data["name"]
+            email = form.cleaned_data["email"]
+            subject = form.cleaned_data["subject"]
+            message = form.cleaned_data["message"]
+            # Send email
+            send_mail(
+                subject,
+                f"{name} wrote, \n\n{message}",
+                email,
+                [settings.DEFAULT_FROM_EMAIL],
+                fail_silently=False,
+            )
+            # Add a success message to be displayed after the redirect
+            messages.success(
+                request, "Thanks for contacting us! We will be in touch soon."
+            )
+            # Redirect to the contact page
+            return redirect(reverse("contact"))
+        # Render the contact page with the invalid form and error messages
+        return render(request, "support/contact.html", {"form": form})
