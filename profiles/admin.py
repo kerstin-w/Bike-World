96d806d6acc8466fac65e9231dea17c7b0ac13f2
Kerstin <kerstinweibeler@gmail.com>
2023-04-26 12:30:22 +0000

    Register Wishlist and ProductReview models with admin site.

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Wishlist, ProductReview
 
-# Register your models here.
+
+@admin.register(ProductReview)
+class ProductReviewAdmin(admin.ModelAdmin):
+    """
+    Admin Panel for ProductReview model with its custom admin configuration
+    """
+
+    list_display = ("product", "user", "rating", "created_at")
+    list_filter = ("product", "user", "created_at")
+    search_fields = ("product__title", "user__username", "review")
+
+
+@admin.register(Wishlist)
+class WishlistAdmin(admin.ModelAdmin):
+    """
+    Admin Panel for WishList model with its custom admin configuration
+    """
+
+    list_display = ("user", "product", "is_product_in_wishlist")
+    list_filter = ("user",)
+    search_fields = ("user__username", "product__title")
