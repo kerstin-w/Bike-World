4873b4f6b8f77c2504d238df9d7a1cad1fbfc84d
Kerstin <kerstinweibeler@gmail.com>
2023-05-01 08:44:21 +0000

    test: Create ProductReview Admin Test Class and test list_display

--- profiles/tests/test_admin.py
+++ profiles/tests/test_admin.py
@@ -19,7 +19,7 @@ class WishlistAdminTest(TestCase):
         self.user = User.objects.create_user(
             username="testuser", password="testpass"
         )
-        product = Product.objects.create(
+        self.product = Product.objects.create(
             title="Test Product",
             sku="test_sku",
             category=Category.objects.create(
@@ -40,7 +40,7 @@ class WishlistAdminTest(TestCase):
             rating=4.5,
         )
         self.wishlist = Wishlist.objects.create(
-            user=self.user, product=product
+            user=self.user, product=self.product
         )
         self.client = Client()
         self.client.login(username="testuser", password="testpass")
@@ -70,4 +70,57 @@ class WishlistAdminTest(TestCase):
         wishlist_admin = WishlistAdmin(Wishlist, self.site)
         self.assertEqual(
             wishlist_admin.search_fields, ("user__username", "product__title")
+        )
+
+
+class ProductReviewAdminTest(TestCase):
+    """
+    Test Case for Product Review Admin
+    """
+
+    def setUp(self):
+        """
+        Test Data
+        """
+        self.user = User.objects.create_user(
+            username="testuser", password="testpass"
+        )
+        self.product = Product.objects.create(
+            title="Test Product",
+            sku="test_sku",
+            category=Category.objects.create(
+                name="Test Category", friendly_name="Test Category"
+            ),
+            description="Test Product Description",
+            wheel_size="26 inches",
+            retail_price=999.99,
+            sale_price=799.99,
+            sale=True,
+            image="path/to/test/image.jpg",
+            brand="Test Brand",
+            bike_type="Test Bike Type",
+            gender=0,
+            material="Test Material",
+            derailleur="Test Derailleur",
+            stock=10,
+            rating=4.5,
+        )
+        self.review = ProductReview.objects.create(
+            product=self.product,
+            user=self.user,
+            review="Test Review",
+            rating=5,
+        )
+        self.client = Client()
+        self.client.login(username="testuser", password="testpass")
+        self.site = AdminSite()
+
+    def test_product_review_admin_list_display(self):
+        """
+        Test that the correct fields are displayed in the list view.
+        """
+        review_admin = ProductReviewAdmin(ProductReview, self.site)
+        self.assertEqual(
+            review_admin.list_display,
+            ("product", "user", "rating", "created_at"),
         )
