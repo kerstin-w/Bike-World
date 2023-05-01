49e72f25d76d1fef433730b301a5a08922b7b5c5
Kerstin <kerstinweibeler@gmail.com>
2023-05-01 07:57:18 +0000

    test: Add test to WishListAdmin Test Class and test list filters and search fields

--- profiles/tests/test_admin.py
+++ profiles/tests/test_admin.py
@@ -23,7 +23,8 @@ class WishlistAdminTest(TestCase):
             title="Test Product",
             sku="test_sku",
             category=Category.objects.create(
-                name="Test Category", friendly_name="Test Category"),
+                name="Test Category", friendly_name="Test Category"
+            ),
             description="Test Product Description",
             wheel_size="26 inches",
             retail_price=999.99,
@@ -36,10 +37,11 @@ class WishlistAdminTest(TestCase):
             material="Test Material",
             derailleur="Test Derailleur",
             stock=10,
-            rating=4.5
+            rating=4.5,
         )
         self.wishlist = Wishlist.objects.create(
-            user=self.user, product=product)
+            user=self.user, product=product
+        )
         self.client = Client()
         self.client.login(username="testuser", password="testpass")
         self.site = AdminSite()
@@ -52,4 +54,20 @@ class WishlistAdminTest(TestCase):
         self.assertEqual(
             wishlist_admin.list_display,
             ("user", "product", "is_product_in_wishlist"),
+        )
+
+    def test_wishlist_admin_list_filter(self):
+        """
+        Test that the correct filters are displayed in the list view.
+        """
+        wishlist_admin = WishlistAdmin(Wishlist, self.site)
+        self.assertEqual(wishlist_admin.list_filter, ("user",))
+
+    def test_wishlist_admin_search_fields(self):
+        """
+        Test that the correct search fields are displayed in the list view.
+        """
+        wishlist_admin = WishlistAdmin(Wishlist, self.site)
+        self.assertEqual(
+            wishlist_admin.search_fields, ("user__username", "product__title")
         )
