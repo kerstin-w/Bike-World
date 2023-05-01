add1085e53bd4a65d50afe31900815a312fc2d37
Kerstin <kerstinweibeler@gmail.com>
2023-05-01 07:54:59 +0000

    test: Create Test Class for WishListAdmin and test list_display

---
+++ profiles/tests/test_admin.py
@@ -0,0 +1,55 @@
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
+from django.contrib.admin.sites import AdminSite
+
+from profiles.models import Wishlist, ProductReview
+from profiles.admin import WishlistAdmin, ProductReviewAdmin
+from products.models import Category, Product
+
+
+class WishlistAdminTest(TestCase):
+    """
+    Test Case for WishList Admin
+    """
+
+    def setUp(self):
+        """
+        Test Data
+        """
+        self.user = User.objects.create_user(
+            username="testuser", password="testpass"
+        )
+        product = Product.objects.create(
+            title="Test Product",
+            sku="test_sku",
+            category=Category.objects.create(
+                name="Test Category", friendly_name="Test Category"),
+            description="Test Product Description",
+            wheel_size="26 inches",
+            retail_price=999.99,
+            sale_price=799.99,
+            sale=True,
+            image="path/to/test/image.jpg",
+            brand="Test Brand",
+            bike_type="Test Bike Type",
+            gender=0,
+            material="Test Material",
+            derailleur="Test Derailleur",
+            stock=10,
+            rating=4.5
+        )
+        self.wishlist = Wishlist.objects.create(
+            user=self.user, product=product)
+        self.client = Client()
+        self.client.login(username="testuser", password="testpass")
+        self.site = AdminSite()
+
+    def test_wishlist_admin_list_display(self):
+        """
+        Test that the correct fields are displayed in the list view.
+        """
+        wishlist_admin = WishlistAdmin(Wishlist, self.site)
+        self.assertEqual(
+            wishlist_admin.list_display,
+            ("user", "product", "is_product_in_wishlist"),
+        )
