d7f1f1ed0a15a615847cce933e16fd7987cfd455
Kerstin <kerstinweibeler@gmail.com>
2023-05-03 14:25:42 +0000

    test: Add test to UserProfileAdminTest and test order count and orders

--- profiles/tests/test_admin.py
+++ profiles/tests/test_admin.py
@@ -10,6 +10,7 @@ from profiles.admin import (
     OrderInline,
 )
 from products.models import Category, Product
+from checkout.models import Order
 
 
 class WishlistAdminTest(TestCase):
@@ -156,6 +157,25 @@ class UserProfileAdminTest(TestCase):
         self.user = User.objects.create_user(
             username="testuser", password="testpass"
         )
+        # delete existing user_profile instance for testuser
+        try:
+            self.user_profile = UserProfile.objects.get(user=self.user)
+            self.user_profile.delete()
+        except UserProfile.DoesNotExist:
+            pass
+
+        # create new user_profile instance for testuser
+        self.user_profile = UserProfile.objects.create(
+            user=self.user,
+            default_full_name="John Doe",
+            default_email="johndoe@test.com",
+            default_phone_number="123456789",
+            default_country="AT",
+            default_postcode="1234",
+            default_town_or_city="Vienna",
+            default_street_address1="Test Street",
+            default_street_address2="Apt 2B",
+        )
 
     def test_user_profile_admin_list_display(self):
         """
@@ -220,4 +240,48 @@ class UserProfileAdminTest(TestCase):
         self.assertListEqual(
             list(self.user_profile_admin.fieldsets),
             expected_fieldsets,
+        )
+
+    def test_orders_count(self):
+        """
+        Test the orders_count returns correct count of orders
+        """
+        # create two orders for the user_profile
+        Order.objects.create(
+            user_profile=self.user_profile,
+            full_name="customer 1",
+            email="customer1@test.com",
+        )
+        Order.objects.create(
+            user_profile=self.user_profile,
+            full_name="customer 2",
+            email="customer2@test.com",
+        )
+
+        expected_count = 2
+        self.assertEqual(
+            self.user_profile_admin.orders_count(self.user_profile),
+            expected_count,
+        )
+
+    def test_orders(self):
+        """
+        Test the orders method returns all the orders of a user profile
+        """
+        order_1 = Order.objects.create(
+            user_profile=self.user_profile,
+            full_name="customer 1",
+            email="customer1@example.com",
+        )
+        order_2 = Order.objects.create(
+            user_profile=self.user_profile,
+            full_name="customer 2",
+            email="customer2@example.com",
+        )
+
+        expected_result = [order_2, order_1]
+        self.assertQuerysetEqual(
+            self.user_profile_admin.orders(self.user_profile),
+            expected_result,
+            transform=lambda x: x,
         )
