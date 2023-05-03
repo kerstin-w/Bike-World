b41cf969b4b370c5c0384314e48d754335669177
Kerstin <kerstinweibeler@gmail.com>
2023-05-03 10:05:29 +0000

    feat: Add OrderInline methode to show order summary in UserProfile

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from django.urls import reverse
 
 from .models import Wishlist, ProductReview, UserProfile
+from checkout.models import Order
 
 
 @admin.register(ProductReview)
@@ -25,6 +28,41 @@ class WishlistAdmin(admin.ModelAdmin):
     search_fields = ("user__username", "product__title")
 
 
+class OrderInline(admin.TabularInline):
+    """
+    Create a new inline class that will be embedded in the UserProfileAdmin
+    and show the Order summary
+    """
+
+    model = Order
+    fields = (
+        "view_order",
+        "date",
+        "order_total",
+        "delivery_cost",
+        "grand_total",
+    )
+    readonly_fields = (
+        "order_number",
+        "date",
+        "order_total",
+        "delivery_cost",
+        "grand_total",
+        "view_order",
+    )
+    can_delete = False
+    extra = 0
+
+    def view_order(self, order):
+        """
+        Return a link to an individual order
+        """
+        url = reverse("admin:checkout_order_change", args=[order.id])
+        return format_html('<a href="{}">{}</a>', url, order.order_number)
+
+    view_order.short_description = "Order Number"
+
+
 @admin.register(UserProfile)
 class UserProfileAdmin(admin.ModelAdmin):
     """
@@ -35,15 +73,15 @@ class UserProfileAdmin(admin.ModelAdmin):
         "user",
         "default_full_name",
         "default_email",
+        "orders_count",
     )
-
+    inlines = [OrderInline]
     search_fields = (
         "user__username",
         "default_email",
         "default_phone_number",
         "default_full_name",
     )
-
     fieldsets = (
         (None, {"fields": ("user", "default_full_name", "default_email")}),
         (
@@ -60,3 +98,28 @@ class UserProfileAdmin(admin.ModelAdmin):
             },
         ),
     )
+
+    def orders_count(self, user_profile):
+        """
+        Define orders_count field that returns the
+        count of orders for each UserProfile
+        """
+        return user_profile.orders.count()
+
+    def orders(self, user_profile):
+        """
+        Define a field that returns all orders for each UserProfile object
+        """
+        return user_profile.orders.all()
+
+    orders.short_description = "Orders"
+
+    def get_inline_instances(self, request, user_profile=None):
+        """
+        Ensure that the OrderInline is only displayed
+        if the userProfile has a related Order
+        """
+        inline_instances = []
+        if user_profile:
+            inline_instances.append(OrderInline(self.model, self.admin_site))
+        return inline_instances
