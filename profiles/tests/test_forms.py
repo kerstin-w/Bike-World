ea7b75b74282db8b5467a887132303e69e7c848c
Kerstin <kerstinweibeler@gmail.com>
2023-05-06 06:43:40 +0000

    test: Add test to UserProfileFormTest and test email already in use

--- profiles/tests/test_forms.py
+++ profiles/tests/test_forms.py
@@ -17,6 +17,13 @@ class UserProfileFormTest(TestCase):
         self.user = User.objects.create_user(
             username="testuser", password="testpass", email="testuser@test.com"
         )
+
+        self.user2 = User.objects.create_user(
+            username="testuser2",
+            password="testpass2",
+            email="testuser2@test.com",
+        )
+
         # delete existing user_profile instance for testuser
         try:
             self.user_profile = UserProfile.objects.get(user=self.user)
@@ -28,7 +35,7 @@ class UserProfileFormTest(TestCase):
         self.user_profile = UserProfile.objects.create(
             user=self.user,
             default_full_name="John Doe",
-            default_email="johndoe@test.com",
+            default_email="testuser@test.com",
             default_phone_number="123456789",
             default_country="AT",
             default_postcode="1234",
@@ -73,7 +80,35 @@ class UserProfileFormTest(TestCase):
         self.assertFalse(form.is_valid())
         self.assertEqual(len(form.errors), 2)
         self.assertIn("default_full_name", form.errors)
-        self.assertIn("This field is required.",
-                      form.errors["default_full_name"])
+        self.assertIn(
+            "This field is required.", form.errors["default_full_name"]
+        )
         self.assertIn("default_email", form.errors)
         self.assertIn("This field is required.", form.errors["default_email"])
+
+    def test_user_profile_form_email_already_in_use(self):
+        """
+        Test that from returns error when User tries to updated
+        email to and email that is already in use
+        """
+        # log in as the first user
+        self.client.login(
+            username=self.user.username, password=self.user.password
+        )
+        # submit form with a email address already in use by user2
+        form_data = {
+            "default_full_name": "John Doe",
+            "default_email": self.user2.email,
+            "default_phone_number": "123456789",
+            "default_country": "AT",
+            "default_postcode": "1234",
+            "default_town_or_city": "Vienna",
+            "default_street_address1": "Test Street",
+            "default_street_address2": "Apt 2B",
+        }
+        form = UserProfileForm(data=form_data, instance=self.user_profile)
+        self.assertFalse(form.is_valid())
+        self.assertTrue("default_email" in form.errors)
+        self.assertEqual(
+            form.errors["default_email"][0], "Email is already in use."
+        )
