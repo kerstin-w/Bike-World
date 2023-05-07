d908b1b87c8c0e15ac5eb3ce95eb738a093157e7
Kerstin <kerstinweibeler@gmail.com>
2023-05-07 13:18:59 +0000

    test: Create Test Case for OrderHistoryView and test valid order number

--- profiles/tests/test_views.py
+++ profiles/tests/test_views.py
@@ -10,7 +10,7 @@ from profiles.models import UserProfile
 from checkout.models import Order, OrderLineItem
 from products.models import Product, Category
 from profiles.forms import UserProfileForm, ProductReviewForm
-from profiles.views import ProfileView, ProfileUpdateView
+from profiles.views import ProfileView, ProfileUpdateView, OrderHistoryView
 
 
 class ProfileViewTest(TestCase):
@@ -300,3 +300,79 @@ class ProfileUpdateViewTest(TestCase):
         self.assertEqual(
             context["user_profile_form"].instance, self.user_profile
         )
+
+
+class OrderHistoryViewTest(TestCase):
+    """
+    Test Case for Order History View
+    """
+
+    def setUp(self):
+        """
+        Test Data
+        """
+        self.client = Client()
+        self.user = User.objects.create_user(
+            username="testuser", password="testpass", email="testuser@test.com"
+        )
+        self.client.login(username="testuser", password="testpass")
+        # delete existing user_profile instance for testuser
+        try:
+            self.user_profile = UserProfile.objects.get(user=self.user)
+            self.user_profile.delete()
+        except UserProfile.DoesNotExist:
+            pass
+        self.user_profile = UserProfile.objects.create(
+            user=self.user,
+            default_full_name=self.user.get_full_name(),
+            default_email=self.user.email,
+        )
+        # create data for testing
+        self.category = Category.objects.create(
+            name="TestCategory", friendly_name="Test Category"
+        )
+        self.country_name = "Austria"
+        self.country_code = "AT"
+        self.product = Product.objects.create(
+            title="Test Product",
+            sku="TESTSKU1234",
+            category=self.category,
+            description="Test description",
+            retail_price=Decimal("499.99"),
+            stock=10,
+            rating=4.5,
+        )
+
+        self.order = Order.objects.create(
+            order_number="12345678",
+            user_profile=self.user_profile,
+            full_name=self.user.get_full_name(),
+            email=self.user.email,
+            phone_number="1234567890",
+            country=self.country_code,
+            postcode="12345",
+            town_or_city="Test Town",
+            street_address1="Test Street 12",
+            street_address2="Test Apt 123",
+            delivery_cost=Decimal("10.00"),
+            order_total=Decimal("499.99"),
+            grand_total=Decimal("509.99"),
+            original_bag="{}",
+            stripe_pid="TestStripePID",
+        )
+
+        self.order_line_item = OrderLineItem.objects.create(
+            order=self.order,
+            product=self.product,
+            quantity=1,
+            lineitem_total=Decimal("499.99"),
+        )
+
+    def test_order_history_view_with_valid_order_number(self):
+        """
+        Test that the view returns a success response with a valid order number
+        """
+        self.client.login(username='testuser', password='testpass')
+        response = self.client.get(reverse('order_history', args=['12345678']))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'checkout/checkout_success.html')
