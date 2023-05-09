85a1fbf23612a97c75ed3fe21f84f12db3c4f406
Kerstin <kerstinweibeler@gmail.com>
2023-05-09 07:47:41 +0000

    test: Create test case for ProductForm and test form valid

---
+++ products/tests/test_forms.py
@@ -0,0 +1,65 @@
+from django.test import TestCase
+from django.core.files.uploadedfile import SimpleUploadedFile
+
+from io import BytesIO
+from PIL import Image
+
+from products.forms import ProductForm
+from products.models import Category, Product
+
+
+class ProductFormTest(TestCase):
+    """
+    Test Case for Product Form
+    """
+
+    @classmethod
+    def setUpTestData(cls):
+        # create a category for the test products
+        cls.category = Category.objects.create(
+            name="TestCategory", friendly_name="Test Category"
+        )
+
+    def setUp(self):
+        """
+        Test Data
+        """
+        self.form_data = {
+            "title": "Test Title",
+            "sku": "12345",
+            "category": self.category.id,
+            "description": "Test Description",
+            "wheel_size": "Test Wheel Size",
+            "retail_price": 50.00,
+            "sale_price": 45.00,
+            "sale": True,
+            "brand": "Test Brand",
+            "bike_type": "Test Bike Type",
+            "gender": 0,
+            "material": "Test Material",
+            "derailleur": "Test Derailleur",
+            "stock": 100,
+            "rating": 3.5,
+        }
+        # create an image to use for file uploads
+        self.image = self.generate_image()
+
+    def generate_image(self):
+        """
+        Utility function to generate a test image for form file uploads
+        """
+        file = BytesIO()
+        image = Image.new("RGBA", size=(50, 50), color=(155, 0, 0))
+        image.save(file, "png")
+        file.name = "test_image.png"
+        file.seek(0)
+        return SimpleUploadedFile(
+            file.name, file.read(), content_type="image/png"
+        )
+
+    def test_product_form_valid_form(self):
+        """
+        Test that form is valid
+        """
+        form = ProductForm(data=self.form_data, files={"image": self.image})
+        self.assertTrue(form.is_valid())
