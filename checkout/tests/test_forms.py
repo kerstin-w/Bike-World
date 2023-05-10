2b1e7ce666f6118d26e7f19cc04b0faabf6bd728
Kerstin <kerstinweibeler@gmail.com>
2023-05-10 11:30:03 +0000

    test: Create Test Case for OrderFrom and test from

---
+++ checkout/tests/test_forms.py
@@ -0,0 +1,57 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+
+from checkout.forms import OrderForm
+from profiles.models import UserProfile
+
+
+# Import the `TestCase` class from Django
+class OrderFormTest(TestCase):
+    """
+    Test Case for Order Form
+    """
+
+    def setUp(self):
+        """
+        Test Data
+        """
+        self.user = User.objects.create_user(
+            username="testuser", password="testpass"
+        )
+        # delete existing user_profile instance for testuser
+        try:
+            self.user_profile = UserProfile.objects.get(user=self.user)
+            self.user_profile.delete()
+        except UserProfile.DoesNotExist:
+            pass
+        self.user_profile = UserProfile.objects.create(
+            user=self.user,
+            default_full_name=self.user.get_full_name(),
+            default_email=self.user.email,
+        )
+
+    def test_order_form(self):
+        """
+        Test that the basic functionality of the OrderForm works correctly
+        """
+        form_data = {
+            "full_name": "Test User",
+            "email": "test@test.com",
+            "phone_number": "123456789",
+            "street_address1": "Test St.",
+            "town_or_city": "Test City",
+            "postcode": "1234",
+            "country": "AT",
+        }
+        form = OrderForm(data=form_data)
+        self.assertTrue(form.is_valid())
+        order = form.save(commit=False)
+        order.user_profile = self.user_profile
+        order.save()
+        self.assertEqual(order.full_name, "Test User")
+        self.assertEqual(order.email, "test@test.com")
+        self.assertEqual(order.phone_number, "123456789")
+        self.assertEqual(order.street_address1, "Test St.")
+        self.assertEqual(order.town_or_city, "Test City")
+        self.assertEqual(order.postcode, "1234")
+        self.assertEqual(order.country, "AT")
