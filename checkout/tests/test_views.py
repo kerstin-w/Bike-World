4226b6a7d3c07d550b99b56e1a34a5e31eb1fa66
Kerstin <kerstinweibeler@gmail.com>
2023-05-10 19:11:54 +0000

    test: Add test to CheckOutViewTest and test post fo valid form

--- checkout/tests/test_views.py
+++ checkout/tests/test_views.py
@@ -63,3 +63,59 @@ class CheckoutViewsTest(TestCase):
         response = self.client.get(self.checkout_url)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "checkout/checkout.html")
+
+    def test_checkout_view_post_valid_order_form(self):
+        """
+        Test POST request to checkout view with valid order form
+        """
+        # Define variables for the Stripe transaction
+        stripe_total = (
+            Decimal(self.bag[str(self.product.id)]) * self.product.retail_price
+        )
+        stripe_total_in_cents = int(stripe_total * 100)
+        # Use mock object to test fake Stripe transaction
+        with patch("stripe.PaymentIntent.create") as stripe_create_mock:
+            # Set return values for the mock object
+            stripe_create_mock.return_value.client_secret = "test_client"
+            stripe_create_mock.return_value.amount = stripe_total_in_cents
+            stripe_create_mock.return_value.currency = "EUR"
+            stripe_create_mock.return_value.status = "succeeded"
+            # Make the request to the checkout view with fake data
+            response = self.client.post(
+                self.checkout_url,
+                {
+                    "full_name": "Test User",
+                    "email": "testuser@example.com",
+                    "phone_number": "1234567890",
+                    "country": "US",
+                    "postcode": "12345",
+                    "town_or_city": "Test City",
+                    "street_address1": "Test Street 1",
+                    "street_address2": "Test Street 2",
+                    "client_secret": "test_client_secret_secret",
+                },
+            )
+            # Assert that the response contains the correct status code,
+            # redirect URL, and creates a new order object with the
+            # correct data
+            self.assertEqual(response.status_code, 302)
+            self.assertEqual(
+                response.url,
+                reverse(
+                    "checkout_success",
+                    args=[Order.objects.latest("id").order_number],
+                ),
+            )
+            self.assertEqual(Order.objects.count(), 1)
+            order = Order.objects.first()
+            self.assertEqual(order.full_name, "Test User")
+            self.assertEqual(order.email, "testuser@example.com")
+            self.assertEqual(order.phone_number, "1234567890")
+            self.assertEqual(order.country, "US")
+            self.assertEqual(order.postcode, "12345")
+            self.assertEqual(order.town_or_city, "Test City")
+            self.assertEqual(order.street_address1, "Test Street 1")
+            self.assertEqual(order.street_address2, "Test Street 2")
+            bag_dict = json.loads(order.original_bag)
+            self.assertEqual(bag_dict, self.session["bag"])
+            self.assertEqual(order.stripe_pid, "test_client")
