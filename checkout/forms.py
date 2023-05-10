4d128f4c5663cccb183d55c4abcba0183a205641
Kerstin <kerstinweibeler@gmail.com>
2023-05-10 10:20:41 +0000

    fix: Missing lables on checkout form. In the  class, add  attributes to each form field to provide a hidden label that is accessible to screen readers and other assistive technologies. The  attribute uses the same value as the  attribute.

--- checkout/forms.py
+++ checkout/forms.py
@@ -40,5 +40,6 @@ class OrderForm(forms.ModelForm):
                 else:
                     placeholder = placeholders[field]
                 self.fields[field].widget.attrs["placeholder"] = placeholder
+                self.fields[field].widget.attrs["aria-label"] = placeholder
             self.fields[field].widget.attrs["class"] = "stripe-style-input"
             self.fields[field].label = False
