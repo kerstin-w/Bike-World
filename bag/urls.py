e6b4e73023927558e2884d1d7f6cec4cb06c406a
Kerstin <kerstinweibeler@gmail.com>
2023-05-14 13:07:26 +0000

    refactor: Break too long line in bag urls

--- bag/urls.py
+++ bag/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', BagView.as_view(), name='view_bag'),
     path('add/<item_id>', AddToBagView.as_view(), name='add_to_bag'),
     path('adjust/<item_id>/', AdjustBagView.as_view(), name='adjust_bag'),
-    path('remove/<item_id>/', RemoveItemFromBagView.as_view(), name='remove_from_bag'),
+    path('remove/<item_id>/', RemoveItemFromBagView.as_view(),
+         name='remove_from_bag'),
 ]
