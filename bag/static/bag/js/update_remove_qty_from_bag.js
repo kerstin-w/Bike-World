5d84f462edf214c6dbe5f33ffa3e05b2abe26012
Kerstin <kerstinweibeler@gmail.com>
2023-05-17 08:16:53 +0000

    refactor: Move script to update/remove qty in bag into separate JS file

---
+++ bag/static/bag/js/update_remove_qty_from_bag.js
@@ -0,0 +1,27 @@
+// Update quantity on click
+$('.update-link').on('click', function (e) {
+    e.preventDefault();
+    const form = $(this).closest('div').find('.update-form');
+    const input = form.find('.qty_input');
+    const quantity = parseInt(input.val());
+    // Check if Input is within allowed range
+    if (quantity >= 1 && quantity <= 99) {
+        form.submit();
+    } else {
+        input[0].setCustomValidity('Please enter a quantity between 1 and 99');
+        input[0].reportValidity();
+    }
+});
+// Remove item and reload on click
+$('.remove-item').on('click', function (e) {
+    e.preventDefault();
+    const itemId = $(this).attr('id').split('remove_')[1];
+    const url = `/bag/remove/${itemId}/`;
+    const data = {
+        'csrfmiddlewaretoken': $('input[name="csrfmiddlewaretoken"]').val(),
+    };
+
+    $.post(url, data, () => {
+        location.reload();
+    });
+})
