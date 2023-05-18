9dd55130651177ff783ddfd7a1e8f2d8e2979ecf
Kerstin <kerstinweibeler@gmail.com>
2023-05-18 09:07:32 +0000

    feat: Create context processor for login

--- profiles/context_processors.py
+++ profiles/context_processors.py
@@ -1,12 +1,22 @@
 from allauth.account.forms import LoginForm, SignupForm
 
 
+class CustomLoginForm(LoginForm):
+    """
+    Custom Login Form to turn of the auto focus
+    """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['login'].widget.attrs.pop('autofocus', None)
+
+
 def login_tag(request):
     """
     Context processor that adds a login form to the context
     of every template that's rendered.
     """
-    return {"logintag": LoginForm()}
+    return {"logintag": CustomLoginForm()}
 
 
 def signup_tag(request):
