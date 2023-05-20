8b46ac22ab6fdc6be07086a7cab41dec5303583f
Kerstin <kerstinweibeler@gmail.com>
2023-05-20 15:22:46 +0000

    improvement: Add aria-label to image in add/edit product form

--- products/forms.py
+++ products/forms.py
@@ -13,7 +13,10 @@ class ProductForm(forms.ModelForm):
         fields = "__all__"
 
     image = forms.ImageField(
-        label='Image', required=False, widget=CustomClearableFileInput)
+        label="Image",
+        required=False,
+        widget=CustomClearableFileInput(attrs={"aria-label": "Select Image"}),
+    )
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -25,4 +28,4 @@ class ProductForm(forms.ModelForm):
 
         self.fields["category"].choices = friendly_names
         for field_name, field in self.fields.items():
-            field.widget.attrs['class'] = 'border-black'
+            field.widget.attrs["class"] = "border-black"
