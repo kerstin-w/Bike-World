c02be787697c72ca659d862f613c4bb4603047c6
Kerstin <kerstinweibeler@gmail.com>
2023-05-20 07:00:24 +0000

    test: Adjsut profiles context-procesor tests to test auto_id

--- profiles/tests/test_context_processors.py
+++ profiles/tests/test_context_processors.py
@@ -1,6 +1,8 @@
 from django.test import RequestFactory, TestCase
 from allauth.account.forms import LoginForm, SignupForm
+
 from profiles.context_processors import login_tag, signup_tag
+from profiles.forms import CustomLoginForm
 
 
 class ContextProcessorTest(TestCase):
@@ -20,7 +22,8 @@ class ContextProcessorTest(TestCase):
         """
         request = self.factory.get("/")
         login_form = login_tag(request)["logintag"]
-        self.assertIsInstance(login_form, LoginForm)
+        self.assertIsInstance(login_form, CustomLoginForm)
+        self.assertEqual(login_form.auto_id, 'login_id_%s')
 
     def test_signup_tag(self):
         """
@@ -29,3 +32,4 @@ class ContextProcessorTest(TestCase):
         request = self.factory.get("/")
         signup_form = signup_tag(request)["signuptag"]
         self.assertIsInstance(signup_form, SignupForm)
+        self.assertEqual(signup_form.auto_id, 'signup_id_%s')
